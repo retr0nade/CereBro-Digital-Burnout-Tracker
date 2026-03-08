a7ade63ff25a846bc6c2c5726e1405cd56c41ad0
retr0nade <148035674+retr0nade@users.noreply.github.com>
2026-03-09 03:32:35 +0530

    tauri frontend removed

--- desktop-app/frontend/src/charts/index.ts
+++
@@ -1,8 +0,0 @@
-// Chart components exports
-export { FocusVsDistractionLine } from './FocusVsDistractionLine';
-export { IdleBreakBar } from './IdleBreakBar';
-export { default as DonutAppUsage } from './DonutAppUsage';
-export { StableContainer } from './StableRechart';
-
-// Utility functions
-export * from './utils';
